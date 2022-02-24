bdec97fca2cd82f42ce82462db833f9a3c497439
stasenash <staslisenash@gmail.com>
2022-02-24 05:06:26 +0500

    веб морда подключена к апи

--- WebShopAdminApplication/Pages/Index.cshtml.cs
+++ WebShopAdminApplication/Pages/Index.cshtml.cs
@@ -6,15 +6,26 @@ namespace WebShopAdminApplication.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IConfigurationRoot _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string Content { get; set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            IConfiguration configRoot)
         {
             _logger = logger;
+            _config = (IConfigurationRoot)configRoot;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            var httpClient = new HttpClient();
+            string adminApiBaseUrl = _config.GetConnectionString("WebShopAdminApiBaseUrl");
+            httpClient.BaseAddress = new Uri(adminApiBaseUrl);
+            var response = await httpClient.GetAsync("/categories/list");
+            var content = await response.Content.ReadAsStringAsync();
+            Content = content;
         }
     }
 }
