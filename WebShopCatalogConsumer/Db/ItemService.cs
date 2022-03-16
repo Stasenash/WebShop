b5b776f94e14efa6cc5340c7a50d180f92911d95
stasenash <staslisenash@gmail.com>
2022-03-17 00:07:35 +0500

    fix item change category

--- WebShopCatalogConsumer/Db/ItemService.cs
+++ WebShopCatalogConsumer/Db/ItemService.cs
@@ -36,14 +36,31 @@ namespace WebShopCatalogConsumer.Db
 
         public void Update(int categoryId, Item item)
         {
-            var category = _categories.Find(x => x.Id == categoryId).FirstOrDefault();
-            if (category == null) throw new KeyNotFoundException();
+            var itemOldCategory = _categories.Find(x => x.Items.Any(y => y.Id == item.Id)).FirstOrDefault();
+            var itemNewCategory = _categories.Find(x => x.Id == categoryId).FirstOrDefault();
+            if (itemNewCategory == null) throw new KeyNotFoundException();
+                        
+            if (itemOldCategory != null && itemOldCategory.Id != categoryId)
+            {
+                // если категория у продукта поменялась
+                var itemOld = itemOldCategory.Items.FirstOrDefault(x => x.Id == item.Id);
 
-            var itemOld = category.Items.FirstOrDefault(x => x.Id == item.Id);
-            category.Items.Remove(itemOld);
-            category.Items.Add(item);
+                itemOldCategory.Items.Remove(itemOld);
+                itemNewCategory.Items.Add(item);
 
-            _categories.ReplaceOne(x => x.Id == category.Id, category);
+                _categories.ReplaceOne(x => x.Id == itemOldCategory.Id, itemOldCategory);
+                _categories.ReplaceOne(x => x.Id == itemNewCategory.Id, itemNewCategory);
+            }
+            else
+            {
+                // если категория не менялась
+                var itemOld = itemNewCategory.Items.FirstOrDefault(x => x.Id == item.Id);
+
+                itemNewCategory.Items.Remove(itemOld);
+                itemNewCategory.Items.Add(item);
+
+                _categories.ReplaceOne(x => x.Id == itemNewCategory.Id, itemNewCategory);
+            }
         }
 
         public void Remove(int categoryId, int itemId)
